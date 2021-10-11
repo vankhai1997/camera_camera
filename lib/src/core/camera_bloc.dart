a8c2416863dbc9785258b7ed3fe06eb6bce81894
Lê Văn Khải <khailv@meeyland.com>
2021-10-11 18:04:52 +0700

    update rxdart

--- lib/src/core/camera_bloc.dart
+++ lib/src/core/camera_bloc.dart
@@ -26,7 +26,7 @@ class CameraBloc {
   final statusStream =
       BehaviorSubject<CameraStatus>.seeded(CameraStatusEmpty());
   CameraStatus get status =>
-      statusStream.value ?? CameraStatusEmpty();
+      statusStream.value;
   set status(CameraStatus status) => statusStream.sink.add(status);
 
   void init() async {
