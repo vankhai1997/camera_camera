52f2b870446ff66666a45903e407fc480f0f007e
Lê Văn Khải <khailv@meeyland.com>
2021-10-11 18:08:29 +0700

    update rxdart

--- lib/src/presentation/widgets/focus_widget.dart
+++ lib/src/presentation/widgets/focus_widget.dart
@@ -47,7 +47,7 @@ class _SquareModePhoto extends CustomClipper<Path> {
     path.addRect(Rect.fromLTWH(0.0, 0.0, size.width, size.height));
     path.addRRect(
       RRect.fromRectAndCorners(
-        Rect.fromLTWH(32, size.height * 2 / 7, size.width - 64, 200),
+        Rect.fromLTWH(32, size.height * 3.5, size.width - 64, 200),
         topLeft: Radius.circular(16),
         topRight: Radius.circular(16),
         bottomLeft: Radius.circular(16),
