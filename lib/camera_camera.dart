fa1c2af6602006e6b4620db90a99cc838f6c064f
Lê Văn Khải <khailv@meeyland.com>
2021-10-11 17:32:10 +0700

    update rxdart

--- lib/camera_camera.dart
+++ lib/camera_camera.dart
@@ -1,5 +1,6 @@
 library camera_camera;
 
 export 'src/presentation/camera_page.dart';
+export 'src/presentation/widgets/focus_widget.dart';
 export 'src/shared/entities/camera_side.dart';
 export 'package:camera/camera.dart';
